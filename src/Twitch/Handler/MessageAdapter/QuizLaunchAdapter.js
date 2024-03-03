8c857fe3950b916a2aebc2e1c8ab3fc066c79175
Jérémy GUERIBA <jeremy.gueriba@gmail.com>
2024-03-03 20:09:00 +0100

    Initialize repository in QuizLaunchAdapter, implement basic handle method

--- src/Twitch/Handler/MessageAdapter/QuizLaunchAdapter.js
+++ src/Twitch/Handler/MessageAdapter/QuizLaunchAdapter.js
@@ -1,7 +1,24 @@
+import QuizRepository from "../../../Database/Repository/QuizRepository.js";
+
+/**
+ * Class QuizLaunchAdapter
+ *
+ * @author GRem
+ */
 export default class QuizLaunchAdapter
 {
+    constructor()
+    {
+        this.repository = new QuizRepository();
+    }
+
     support(context, message)
     {
         return context.username === 'gremlive' && message.trim() === '!quiz';
+    }
+
+    handle(context, message)
+    {
+        this.repository.getRandomQuiz();
     }
 }
