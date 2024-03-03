0e302cd55c5dbf7ced5905918662994852d55fdf
Jérémy GUERIBA <jeremy.gueriba@gmail.com>
2024-03-03 23:42:58 +0100

    Implement query to get random quiz in QuizRepository

--- src/Database/Repository/QuizRepository.js
+++ src/Database/Repository/QuizRepository.js
@@ -1,3 +1,4 @@
+import QuestionFactory from "../../Entity/Factory/QuestionFactory.js";
 import ConnectionInitializer from "../ConnectionInitalizer.js";
 
 export default class QuizRepository
@@ -5,8 +6,20 @@ export default class QuizRepository
     async getRandomQuiz()
     {
         let connection = await ConnectionInitializer.getConnection();
-        let [result] = await connection.execute("SELECT * FROM question");
+        let [result] = await connection.execute(
+            "WITH randomQuestion AS (SELECT * FROM question WHERE submit_at IS NULL ORDER BY RAND() LIMIT 1)" +
+            "SELECT\n" +
+            "   q.id,\n" +
+            "   q.content,\n" +
+            "   q.answer_precision,\n" +
+            "   a.answer_prefix,\n" +
+            "   a.is_answer,\n" +
+            "   a.propose\n" +
+            "FROM randomQuestion q\n" +
+            "JOIN answer a\n" +
+            "   ON q.id = a.question_id"
+        );
 
-        return result;
+        return QuestionFactory.create(result);
     }
 }
