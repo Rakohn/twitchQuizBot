5d792d4a5358609c86757fa815f2b5ccd998c41b
Jérémy GUERIBA <jeremy.gueriba@gmail.com>
2024-03-06 00:51:41 +0100

    Complete quiz launch behavior

--- src/Twitch/Handler/MessageAdapter/QuizLaunchAdapter.js
+++ src/Twitch/Handler/MessageAdapter/QuizLaunchAdapter.js
@@ -1,6 +1,8 @@
 import QuestionNotFoundError from "../../../Database/Repository/Error/QuestionNotFoundError.js";
 import QuizRepository from "../../../Database/Repository/QuizRepository.js";
 import NoAnswerError from "../../../Entity/Error/NoAnswerError.js";
+import QuizEndEvent from "../../../Event/QuizEndEvent.js";
+import Quiz from "../../Quiz.js";
 
 /**
  * Class QuizLaunchAdapter
@@ -25,17 +27,31 @@ export default class QuizLaunchAdapter
      * @param {string} message
      * @param {import("tmi.js").Actions} client
      */
-    handle(target, context, message, client)
+    async handle(target, context, message, client)
     {
         try{
-            this.repository.getRandomQuiz();
+            const question = await this.repository.getRandomQuiz();
+
+            Quiz.start(question);
+
+            let message = question.content + "\n";
+            question.answers.forEach(answer => {
+                message += "!" + answer.answerPrefix + " " + answer.propose;
+            });
+
+            const endEvent = new QuizEndEvent(target, client);
+
+            client.say(target, message);
+
+            setTimeout(endEvent.onTimeout.bind(endEvent), 20000);
         } catch (error) {
             if (error instanceof QuestionNotFoundError) {
                 message = "On a plus de question en stock patron !";
             } else if (error instanceof NoAnswerError) {
-                message = "On a un léger problème patron qui requiert votre attention.";
+                message = "On a un léger problème, patron, qui requiert votre attention.";
             } else {
-                message = "C'est le bordayle patron, rien ne va annulez tout !";
+                message = "C'est le bordayle patron, rien ne va, annulez tout !";
+                console.log(error);
             }
 
             client.say(target, message);
