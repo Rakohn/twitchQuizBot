7f2d41831175423500fc65506ce515e3f32d15d6
Jérémy GUERIBA <jeremy.gueriba@gmail.com>
2024-03-09 18:47:11 +0100

    Refactor app launch

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-import tmi from 'tmi.js';
-import MessageHandler from './src/Twitch/Handler/MessageHandler.js';
-import MessageHandlerFactory from './src/Twitch/Handler/MessageHandlerFactory.js';
-import ConnectionHandler from './src/Twitch/Handler/ConnectionHandler.js';
+import TwitchBotLauncher from "./src/Twitch/TwitchBotLauncher.js";
 
-const twitchOptions = {
-    identity: {
-        username: process.env.BOT_NAME,
-        password: process.env.TWITCH_OAUTH_TOKEN
-    },
-    channels: [
-        process.env.CHANNEL
-    ]
-};
-
-const client = new tmi.client(twitchOptions);
-const messageHandler = MessageHandlerFactory.create(client);
-
-client.on('message', messageHandler.onMessage.bind(messageHandler));
-client.on('connected', ConnectionHandler.onConnectedEvent);
-client.connect();
+TwitchBotLauncher.launch();
